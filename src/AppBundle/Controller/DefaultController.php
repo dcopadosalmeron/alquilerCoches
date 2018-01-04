658677a9e43c1d7b9f0b774af58980c48800ad2b
Daniel Copado Salmerón <danics95@hotmail.es>
2018-01-04 20:50:43 +0100

    Realizado primer formulario (Aún quedan algunas validaciones), se ha añadido un plugin jQuery para selección de fecha

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,73 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Ciudad;
+use Doctrine\ORM\EntityRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="inicio")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+
+        //Primer formulario
+        $form = $this->createFormBuilder()
+            ->add('fechaInicio', null, array(
+                'label' => 'Fecha Inicial',
+                'constraints' => array(
+                    new NotBlank(array(
+                        'message'=>'La fecha es obligatoria.'
+                    ))
+                ),
+                'attr' => array(
+                    'class' => 'fecha',
+                    'placeholder' => 'Seleccione una fecha inicial',
+                )
+            ))
+            ->add('fechaFin', null, array(
+                'label' => 'Fecha Final',
+                'constraints' => array(
+                    new NotBlank(array(
+                        'message'=>'La fecha es obligatoria.'
+                    ))
+                ),
+                'attr' => array(
+                    'class' => 'fecha',
+                    'placeholder' => 'Seleccione una fecha final',
+                )
+            ))
+            ->add('roomGroups', EntityType::class, array(
+                'label' => 'Ciudad',
+                'class' => Ciudad::class,
+                'constraints' => array(
+                    new NotBlank(array(
+                        'message'=>'La ciudad es obligatoria.'
+                    ))
+                ),
+                'choice_label' => function ($ciudad) {
+                    return $ciudad->getNombre().'('.$ciudad->getProvincia().')';
+                },
+                'placeholder' => 'Seleccione una ciudad',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->orderBy('c.nombre', 'ASC')
+                        ->join('c.coches', 'co');
+                }
+            ));
+
+        $form = $form->getForm()->handleRequest($request);
+
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'form' => $form->createView()
         ]);
     }
 }
